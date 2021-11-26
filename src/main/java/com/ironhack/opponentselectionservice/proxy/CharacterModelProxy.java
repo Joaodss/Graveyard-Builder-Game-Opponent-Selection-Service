de0ec95e4b4412a5e81125424d26b6490117212b
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-26 16:02:20 +0000

    Add character proxy

---
+++ src/main/java/com/ironhack/opponentselectionservice/proxy/CharacterModelProxy.java
@@ -0,0 +1,23 @@
+package com.ironhack.opponentselectionservice.proxy;
+
+import com.ironhack.opponentselectionservice.dto.CharacterDTO;
+import com.ironhack.opponentselectionservice.dto.NewCharacterDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "character-model-service")
+@RequestMapping("/api/v1/characters")
+public interface CharacterModelProxy {
+
+    @GetMapping("/party/{username}")
+    List<CharacterDTO> getCharactersByUserUsernameActive(@PathVariable String username);
+
+    @PostMapping("/create")
+    CharacterDTO createCharacter(@RequestBody NewCharacterDTO newCharacterDTO);
+
+    @PutMapping("/update")
+    CharacterDTO updateCharacter(@RequestBody CharacterDTO updateCharacterDTO);
+
+}
